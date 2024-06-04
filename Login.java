1b4107b0c239e9107b7c215202f5862791ec6f64
Gabriel Machado Carrara Pimentel <126582480+GabrielMachadoCP@users.noreply.github.com>
2024-06-04 11:21:31 -0300

    Classe Login adicionada

---
+++ Login.java
@@ -0,0 +1,36 @@
+package br.com.fiap.bean;
+
+
+public class Login extends Cadastro {
+
+
+    // Construtores
+    public Login() {}
+
+    public Login(String user, long cpf, String senha) {
+        super.setUser(user);
+        super.setCpf(cpf);
+        super.setSenha(senha);
+    }
+    
+    //Sobrecarga de métodos
+    
+    // Método para validar login usando o usuário
+    public boolean validaLogin(String user, String senha) {
+        if (super.getUser().equals(user) && super.getSenha().equals(senha)) {
+            return true; // Login válido
+        } else {;
+            return false; // Login inválido
+        }
+    }
+
+    // Método para validar login usando o CPF
+    public boolean validaLogin(long cpf, String senha) {
+        if (super.getCpf() == cpf && super.getSenha().equals(senha)) {
+            return true; // Login válido
+        } else {;
+            return false; // Login inválido
+        }
+    }
+
+}
